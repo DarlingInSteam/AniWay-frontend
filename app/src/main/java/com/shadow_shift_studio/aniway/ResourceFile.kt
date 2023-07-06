c2324b8c9cfb5ac1b9210c91d86344816aa6cf2a
darlingPc <rombk2711@gmail.com>
2023-07-07 03:33:23 +0700

    add preview list

--- app/src/main/java/com/shadow_shift_studio/aniway/ResourceFile.kt
+++ app/src/main/java/com/shadow_shift_studio/aniway/ResourceFile.kt
@@ -2,6 +2,8 @@ package com.shadow_shift_studio.aniway
 
 val CatalogButtonName: String = "Каталог"
 val CatalogSearchBarText: String = "Что хотите найти?"
+val CatalogSortingButton: String = "Сортировка"
+val CatalogFilterButtonText: String = "Фильтры"
 
 val TopsButtonName: String = "Топы"
 
--- app/src/main/java/com/shadow_shift_studio/aniway/screen/CatalogScreen.kt
+++ app/src/main/java/com/shadow_shift_studio/aniway/screen/CatalogScreen.kt
@@ -1,27 +1,46 @@
 package com.shadow_shift_studio.aniway.screen
 
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyGridState
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.FilterList
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Sort
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.SearchBar
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.shadow_shift_studio.aniway.ui.theme.md_theme_dark_surface_container_high
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.shadow_shift_studio.aniway.CatalogSearchBarText
+import com.shadow_shift_studio.aniway.manga_card.MangaPreviewCard
 
+@Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CatalogScreen() {
@@ -31,10 +50,9 @@ fun CatalogScreen() {
     val horizontalPadding = animateDpAsState(if (expanded) 0.dp else 23.dp).value
     val verticalPadding = animateDpAsState(if (expanded) 0.dp else 11.dp).value
 
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = horizontalPadding, vertical = verticalPadding)
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(horizontal = horizontalPadding, vertical = verticalPadding)
     ) {
         SearchBar(
             modifier = Modifier.fillMaxWidth(),
@@ -52,7 +70,7 @@ fun CatalogScreen() {
                 expanded = it
             },
             placeholder = {
-                Text(text = CatalogSearchBarText)
+                Text(text = com.shadow_shift_studio.aniway.CatalogSearchBarText)
             },
             leadingIcon = {
                 Icon(imageVector = Icons.Default.Search, contentDescription = "Search icon")
@@ -71,9 +89,72 @@ fun CatalogScreen() {
                 }
             },
         ) {
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
 
+            ExtendedFloatingActionButton(
+                onClick = { /*TODO*/
+
+                },
+                shape = RoundedCornerShape(28.dp),
+                modifier = Modifier
+                    .width(146.dp)
+                    .height(40.dp),
+                containerColor = md_theme_dark_surface_container_high
+            ) {
+                Icon(
+                    Icons.Default.Sort,
+                    "Sort icon",
+                    modifier = Modifier
+                        .width(18.dp)
+                        .height(18.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = com.shadow_shift_studio.aniway.CatalogSortingButton)
+            }
+            ExtendedFloatingActionButton(
+                onClick = { /*TODO*/
+
+                },
+                shape = RoundedCornerShape(28.dp),
+                modifier = Modifier
+                    .width(127.dp)
+                    .height(40.dp),
+                containerColor = md_theme_dark_surface_container_high
+            ) {
+                Icon(
+                    Icons.Default.FilterList,
+                    "Sort icon",
+                    modifier = Modifier
+                        .width(18.dp)
+                        .height(18.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = com.shadow_shift_studio.aniway.CatalogFilterButtonText)
+            }
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            LazyVerticalGrid(
+                GridCells.Fixed(3),
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                items(count = 450) { index ->
+                    MangaPreviewCard()
+                }
+            }
         }
     }
 }
+
 
 
