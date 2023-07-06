71ab00c55f2709da4782bac483ab3b94267467fc
darlingPc <rombk2711@gmail.com>
2023-07-06 23:36:41 +0700

    add search bar

--- app/src/main/java/com/shadow_shift_studio/aniway/ResourceFile.kt
+++ app/src/main/java/com/shadow_shift_studio/aniway/ResourceFile.kt
@@ -1,6 +1,11 @@
 package com.shadow_shift_studio.aniway
 
 val CatalogButtonName: String = "Каталог"
+val CatalogSearchBarText: String = "Что хотите найти?"
+
 val TopsButtonName: String = "Топы"
+
 val MyButtonName: String = "Моё"
+
 val ProfileButtonName: String = "Профиль"
+
--- app/src/main/java/com/shadow_shift_studio/aniway/screen/CatalogScreen.kt
+++ app/src/main/java/com/shadow_shift_studio/aniway/screen/CatalogScreen.kt
@@ -1,22 +1,79 @@
 package com.shadow_shift_studio.aniway.screen
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.shadow_shift_studio.aniway.CatalogSearchBarText
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CatalogScreen(){
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
+fun CatalogScreen() {
+    var searchText by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
+    val horizontalPadding = animateDpAsState(if (expanded) 0.dp else 23.dp).value
+    val verticalPadding = animateDpAsState(if (expanded) 0.dp else 11.dp).value
 
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = horizontalPadding, vertical = verticalPadding)
+    ) {
+        SearchBar(
+            modifier = Modifier.fillMaxWidth(),
+            query = searchText,
+            onQueryChange = {
+                searchText = it
+            },
+            onSearch = {
+                active = false
+                expanded = false
+            },
+            active = active,
+            onActiveChange = {
+                active = it
+                expanded = it
+            },
+            placeholder = {
+                Text(text = CatalogSearchBarText)
+            },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Search, contentDescription = "Search icon")
+            },
+            trailingIcon = {
+                if (active) {
+                    Icon(
+                        modifier = Modifier.clickable {
+                            searchText = ""
+                            active = false
+                            expanded = false
+                        },
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "Close icon"
+                    )
+                }
+            },
+        ) {
+
+        }
     }
 }
+
 
